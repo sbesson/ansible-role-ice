393953425b76de54b230c3d7528011fd4a8a5033
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-05-23 11:33:53 +0100

    remove ref to ice 3.7

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Role Variables
 --------------
 
 Required:
-- `ice_version`: The Ice version, either 3.6 or 3.7
+- `ice_version`: The Ice version supported 3.6
 
 Optional (expert users only):
 - `ice_install_devel`: Install Ice development packages, default `True`
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,24 +10,15 @@ def test_ice_version(host):
     hostname = host.backend.get_hostname()
     assert host.exists('icegridnode')
     c = host.run('icegridnode --version')
-    if hostname == 'ice-37':
-        assert c.stderr.startswith('3.7.')
-    else:
-        assert c.stderr.startswith('3.6.')
+    assert c.stderr.startswith('3.6.')
 
 
 def test_icepy_version(host):
     hostname = host.backend.get_hostname()
     c = host.run('python -c "import Ice; print Ice.stringVersion()"')
-    if hostname == 'ice-37':
-        assert c.stdout.startswith('3.7.')
-    else:
-        assert c.stdout.startswith('3.6.')
+    assert c.stdout.startswith('3.6.')
 
 
 def test_ice_devel(host):
     hostname = host.backend.get_hostname()
-    if hostname == 'ice-37':
-        assert not host.package('ice-c++-devel').is_installed
-    else:
-        assert host.package('ice-all-devel').is_installed
+    assert host.package('ice-all-devel').is_installed
