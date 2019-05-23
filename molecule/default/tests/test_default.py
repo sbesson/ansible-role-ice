382d9e90cf191af9f4b165efc6385114dc3cc08f
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-05-23 15:03:23 +0100

    update test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,18 +7,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_ice_version(host):
-    hostname = host.backend.get_hostname()
     assert host.exists('icegridnode')
     c = host.run('icegridnode --version')
     assert c.stderr.startswith('3.6.')
 
 
 def test_icepy_version(host):
-    hostname = host.backend.get_hostname()
     c = host.run('python -c "import Ice; print Ice.stringVersion()"')
     assert c.stdout.startswith('3.6.')
 
 
 def test_ice_devel(host):
-    hostname = host.backend.get_hostname()
     assert host.package('ice-all-devel').is_installed
